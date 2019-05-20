194d2135fda118f28aeb5c7cf5e784b69f73c6cd
Mintae Lee <lee.mintae@codeimmersives.com>
2019-05-20 11:01:59 -0400

    initial commit with skeleton js

---
+++ projects/calculator/main.js
@@ -0,0 +1,44 @@
+window.onload = init;
+
+function init() {
+    document.querySelector('[data-action=add]')
+        .addEventListener('click', calcAdd);
+    document.querySelector('[data-action=sumbtract]')
+        .addEventListener('click', calcSubtract);
+    document.querySelector('[data-action=multiply]')
+        .addEventListener('click', calcMultiply);
+    document.querySelector('[data-action=divide]')
+        .addEventListener('click', calcDivide);
+    document.querySelector('[data-action=decimal]')
+        .addEventListener('click', addDecimal);
+    document.querySelector('[data-action=clear]')
+        .addEventListener('click', clearCalc);
+    document.querySelector('[data-action=calculate]')
+        .addEventListener('click', calculate);
+
+        
+        
+    
+}
+
+function calcAdd(event){
+
+}
+function calcSubtract(event){
+
+}
+function calcMultiply(event){
+
+}
+function calcDivide(event){
+
+}
+function addDecimal(event){
+
+}
+function clearCalc(event){
+
+}
+function calculate(event){
+
+}
